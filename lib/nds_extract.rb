a4a869b1529e7beed8a2a7daa60a01ea4f8c6dd4
Parker Bacall <pbacall2@gmail.com>
2019-10-29 01:34:58 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -97,6 +97,7 @@ inner_array = [{title: 0, director_name: 10}]
     while inner_index < source[index][:movies].length do
     inner_array[index][:director_name] = source[index][:name]
     inner_array[index][:title] = source[index][:movies][inner_index][:title]
+     pp inner_array
     index += 1
   end
   inner_index += 1
