f4efd10374450742f29d189dc0c21a01ba4dcd27
Parker Bacall <pbacall2@gmail.com>
2019-10-29 01:53:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -84,8 +84,8 @@ def movies_with_directors_set(source)
 #pp source
  #pp source[0][:movies][0][:title]
 
-inner_array = [{title: 0, director_name: 10}]
-outer_array = [inner_array]
+inner_array = [{title: 0, director_name: 10},]
+#outer_array = [inner_array]
   # INPUT:
   # * source: An Array of Hashes containing director information including
   # :name and :movies
